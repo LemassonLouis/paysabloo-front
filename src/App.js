4ae93aac5fd9d0679e77fc2cae6ac126af72961e
LEMASSON Louis <louis.lemasson.pro@gmail.com>
2023-12-11 14:10:27 +0100

    Test Axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [domains, setDomains] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect()");
+    axios.get("http://localhost:1337/api/domains").then(result => {
+      console.log("result", result);
+      const domains = result.data.data.map(elm => {
+        elm.attributes.id = elm.id;
+        return elm.attributes;
+      });
+      setDomains(domains);
+    });
+  });
+
   return (
     <div className="App">
       <h1>Hello world !</h1>
+      {domains.map(domain => {
+        return <p key={domain.id}>{domain.title}</p>
+      })}
     </div>
   );
 }
